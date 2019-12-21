bbc113ab09c22636fdeeb9a512be6fde17f6a4e9
kmtusher97 <kmtusher97@gmail.com>
2019-12-22 05:53:28 +0600

    Rest controller for Syllabus services

--- syllabus/src/main/java/com/manager/syllabus/syllabus/controller/SyllabusEditorController.java
+++ syllabus/src/main/java/com/manager/syllabus/syllabus/controller/SyllabusEditorController.java
@@ -1,11 +1,9 @@
 package com.manager.syllabus.syllabus.controller;
 
+import com.manager.syllabus.syllabus.model.bundle.Course;
 import com.manager.syllabus.syllabus.service.syllabus.SyllabusServices;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "syllabus/edit")
@@ -14,11 +12,55 @@ public class SyllabusEditorController {
     @Autowired
     private SyllabusServices syllabusServices;
 
-    @GetMapping("/{syllabusName}/add/year")
+    @PostMapping("/{syllabusName}/add/year")
     public String addYear(
             @PathVariable("syllabusName") String syllabusName
     ) {
-        syllabusServices.addYear(syllabusName);
-        return "added";
+        return syllabusServices.addYear(syllabusName);
+    }
+
+    @DeleteMapping("/{syllabusName}/delete/year/{yearId}")
+    public String deleteYear(
+            @PathVariable("syllabusName") String syllabusName,
+            @PathVariable("yearId") Integer yearId
+    ) {
+        return syllabusServices.deleteYearById(syllabusName, yearId);
+    }
+
+    @PostMapping("/{syllabusName}/{yearId}/add/semester")
+    public String addSemesterIntoYear(
+            @PathVariable("syllabusName") String syllabusName,
+            @PathVariable("yearId") Integer yearId
+    ) {
+        return syllabusServices.addSemesterIntoYear(syllabusName, yearId);
+    }
+
+    @DeleteMapping("/{syllabusName}/{yearId}/delete/semester/{semesterId}")
+    public String deleteSemesterFromYear(
+            @PathVariable("syllabusName") String syllabusName,
+            @PathVariable("yearId") Integer yearId,
+            @PathVariable("semesterId") Integer semesterId
+    ) {
+        return syllabusServices.deleteSemesterFromYear(syllabusName, yearId, semesterId);
+    }
+
+    @PostMapping("/{syllabusName}/{yearId}/{semesterId}/add/course")
+    public String addCourseIntoSemester(
+            @PathVariable("syllabusName") String syllabusName,
+            @PathVariable("yearId") Integer yearId,
+            @PathVariable("semesterId") Integer semesterId,
+            @RequestBody Course course
+    ) {
+        return syllabusServices.addCourseIntoSemester(syllabusName, yearId, semesterId, course);
+    }
+
+    @DeleteMapping("/{syllabusName}/{yearId}/{semesterId}/delete/course/{courseCode}")
+    public String deleteCourseFromSemesterByCourseCode(
+            @PathVariable("syllabusName") String syllabusName,
+            @PathVariable("yearId") Integer yearId,
+            @PathVariable("semesterId") Integer semesterId,
+            @PathVariable("courseCode") String courseCode
+    ) {
+        return syllabusServices.deleteCourseFromSemesterByCourseCode(syllabusName, yearId, semesterId, courseCode);
     }
 }
