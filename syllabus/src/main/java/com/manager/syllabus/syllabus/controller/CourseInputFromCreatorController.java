dbfcda4d59fb42c1aa27b28cb05624c12e706ca1
kmtusher97 <kmtusher97@gmail.com>
2020-01-11 22:34:21 +0600

    Add new form section request

--- syllabus/src/main/java/com/manager/syllabus/syllabus/controller/CourseInputFromCreatorController.java
+++ syllabus/src/main/java/com/manager/syllabus/syllabus/controller/CourseInputFromCreatorController.java
@@ -1,10 +1,26 @@
 package com.manager.syllabus.syllabus.controller;
 
+import com.manager.syllabus.syllabus.service.syllabus.CourseInputFormServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(path = "create_form")
+@RequestMapping(path = "syllabus/create_form")
 public class CourseInputFromCreatorController {
 
+    @Autowired
+    private CourseInputFormServices courseInputFormServices;
+
+    @GetMapping("/{syllabusName}/{courseTypeName}/add_new_section")
+    public String addNewFormSection(
+            @PathVariable("syllabusName") String syllabusName,
+            @PathVariable("courseTypeName") String courseTypeName
+    ) {
+        return courseInputFormServices.addNewFormSection(
+                syllabusName, courseTypeName
+        );
+    }
 }
