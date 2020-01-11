8a0408c0dd28d9242bf88d278c130533d994374a
Kamrul Hasan Tusher <kmtusher97@gmail.com>
2020-01-11 19:53:25 +0600

    Create CourseInputFormCreatorController Rest Controller

---
+++ syllabus/src/main/java/com/manager/syllabus/syllabus/controller/CourseInputFromCreatorController.java
@@ -0,0 +1,10 @@
+package com.manager.syllabus.syllabus.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "create_form")
+public class CourseInputFromCreatorController {
+
+}
