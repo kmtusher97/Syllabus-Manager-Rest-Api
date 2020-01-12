266f29218ab8cb500d314778174fa7faccafbe26
kmtusher97 <kmtusher97@gmail.com>
2020-01-13 00:54:51 +0600

    Update Service methods for addField() and deleteField()

--- syllabus/src/main/java/com/manager/syllabus/syllabus/service/syllabus/FormContentTableServices.java
+++ syllabus/src/main/java/com/manager/syllabus/syllabus/service/syllabus/FormContentTableServices.java
@@ -1,6 +1,7 @@
 package com.manager.syllabus.syllabus.service.syllabus;
 
 import com.manager.syllabus.syllabus.model.bundle.CourseInputForm;
+import com.manager.syllabus.syllabus.model.contents.Table;
 import com.manager.syllabus.syllabus.model.contents.TableRow;
 import com.manager.syllabus.syllabus.service.basex.BaseXServices;
 import com.manager.syllabus.syllabus.service.jaxb.JAXBServices;
@@ -39,6 +40,61 @@ public class FormContentTableServices {
         ).equals("true");
     }
 
+    /**
+     * @param syllabusName
+     * @param courseTypeName
+     * @param sectionSerialId
+     * @return
+     */
+    public Table getTableFromFormSection(
+            String syllabusName, String courseTypeName, Integer sectionSerialId
+    ) {
+        return (Table) jaxbServices.xmlStringToObject(
+                baseXServices.read(
+                        "//syllabus[@name=\""
+                                + syllabusName + "\"]//courseType[@name=\"" + courseTypeName
+                                + "\"]//courseInputForm//courseInputFormSection[@serialId=\""
+                                + sectionSerialId + "\"]//table"
+                ),
+                new Table()
+        );
+    }
+
+    /**
+     * @param syllabusName
+     * @param courseTypeName
+     * @param sectionSerialId
+     */
+    private void deleteTableFromFormSection(
+            String syllabusName, String courseTypeName, Integer sectionSerialId
+    ) {
+        baseXServices.write(
+                "delete node //syllabus[@name=\""
+                        + syllabusName + "\"]//courseType[@name=\"" + courseTypeName
+                        + "\"]//courseInputForm//courseInputFormSection[@serialId=\""
+                        + sectionSerialId + "\"]//table"
+        );
+    }
+
+    /**
+     * @param table
+     * @param syllabusName
+     * @param courseTypeName
+     * @param sectionSerialId
+     */
+    public void saveTableInFormSection(
+            Table table, String syllabusName, String courseTypeName, Integer sectionSerialId
+    ) {
+        baseXServices.write(
+                "insert node "
+                        + jaxbServices.objectToXmlString(table, false)
+                        + " into //syllabus[@name=\""
+                        + syllabusName + "\"]//courseType[@name=\"" + courseTypeName
+                        + "\"]//courseInputForm//courseInputFormSection[@serialId=\""
+                        + sectionSerialId + "\"]"
+
+        );
+    }
 
     /**
      * @param syllabusName
@@ -48,32 +104,30 @@ public class FormContentTableServices {
     public void addNewFieldInTableOfFormSection(
             String syllabusName, String courseTypeName, Integer sectionSerialId
     ) {
-
-        baseXServices.write(
-                "insert node  <field>New Field</field> as last into //syllabus[@name=\""
-                        + syllabusName + "\"]//courseType[@name=\"" + courseTypeName
-                        + "\"]//courseInputForm//courseInputFormSection[@serialId=\""
-                        + sectionSerialId + "\"]//table[@contentId=\""
-                        + sectionSerialId + "\"]//fields"
+        Table table = getTableFromFormSection(
+                syllabusName, courseTypeName, sectionSerialId
         );
+        deleteTableFromFormSection(syllabusName, courseTypeName, sectionSerialId);
+        table.addNewField();
+        saveTableInFormSection(table, syllabusName, courseTypeName, sectionSerialId);
     }
 
     /**
      * @param syllabusName
      * @param courseTypeName
      * @param sectionSerialId
-     * @param fieldId
+     * @param fieldId         1 base index, send to Table class by subtracting 1
      */
     public void deleteFieldFromTableInFormSectionBySerialNo(
             String syllabusName, String courseTypeName, Integer sectionSerialId, Integer fieldId
     ) {
-        baseXServices.write(
-                "delete node //syllabus[@name=\""
-                        + syllabusName + "\"]//courseType[@name=\"" + courseTypeName
-                        + "\"]//courseInputForm//courseInputFormSection[@serialId=\""
-                        + sectionSerialId + "\"]//table[@contentId=\""
-                        + sectionSerialId + "\"]//fields//field[" + fieldId + "]"
+        Table table = getTableFromFormSection(
+                syllabusName, courseTypeName, sectionSerialId
         );
+        deleteTableFromFormSection(syllabusName, courseTypeName, sectionSerialId);
+
+        table.deleteField(fieldId - 1);
+        saveTableInFormSection(table, syllabusName, courseTypeName, sectionSerialId);
     }
 
     /**
