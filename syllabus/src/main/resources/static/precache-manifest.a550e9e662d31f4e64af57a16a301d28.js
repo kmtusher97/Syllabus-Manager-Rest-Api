6a3036c635906627be15b9350b2e2dfcdc721ef2
kmtusher97 <kmtusher97@gmail.com>
2020-01-30 20:46:19 +0600

    View aded and clean install

---
+++ syllabus/src/main/resources/static/precache-manifest.a550e9e662d31f4e64af57a16a301d28.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d282a94e78ca610cd9579fd9f385b98",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4423195e4ff07ba7a6fd",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "0805fdb29395c35f8bb6",
+    "url": "/static/css/main.fbf053fa.chunk.css"
+  },
+  {
+    "revision": "691b08b723a843cb19e6",
+    "url": "/static/js/10.c6b312d8.chunk.js"
+  },
+  {
+    "revision": "08b673e4517f6efdc058",
+    "url": "/static/js/11.202f13b1.chunk.js"
+  },
+  {
+    "revision": "c53c7a30745ebc480b27",
+    "url": "/static/js/12.bb0ab6a0.chunk.js"
+  },
+  {
+    "revision": "4423195e4ff07ba7a6fd",
+    "url": "/static/js/2.39040c71.chunk.js"
+  },
+  {
+    "revision": "6951e5822a376f3683b989464a621d5c",
+    "url": "/static/js/2.39040c71.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bdce41161c61cd034122",
+    "url": "/static/js/3.c6ca158e.chunk.js"
+  },
+  {
+    "revision": "ba9d84414eac4d6e8778",
+    "url": "/static/js/4.dce44db2.chunk.js"
+  },
+  {
+    "revision": "842974c15ff66c3cbfa0",
+    "url": "/static/js/5.180cfc5e.chunk.js"
+  },
+  {
+    "revision": "c032178e2b58cd03d402",
+    "url": "/static/js/6.22243a63.chunk.js"
+  },
+  {
+    "revision": "36606b1dc57a9b863824",
+    "url": "/static/js/7.ec9e3b67.chunk.js"
+  },
+  {
+    "revision": "b96395e545c31dfcffbd",
+    "url": "/static/js/8.06583331.chunk.js"
+  },
+  {
+    "revision": "71afdc36fd679f88ae6f",
+    "url": "/static/js/9.e68c013b.chunk.js"
+  },
+  {
+    "revision": "0805fdb29395c35f8bb6",
+    "url": "/static/js/main.d6b9e67f.chunk.js"
+  },
+  {
+    "revision": "6d2220808420030cda96",
+    "url": "/static/js/runtime-main.5f72f1a8.js"
+  },
+  {
+    "revision": "5e2a2de10de28f92a470b2c95055139e",
+    "url": "/static/media/Jahangirnagar_University_logo.5e2a2de1.png"
+  }
+]);
---
+++ syllabus/src/main/resources/static/static/js/10.c6b312d8.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpcseju-office-management-app"]=this["webpackJsonpcseju-office-management-app"]||[]).push([[10],{89:function(e,t,n){"use strict";n.r(t);var a=n(8),c=n(9),u=n(12),r=n(10),i=n(11),s=n(0),o=n.n(s),p=function(e){function t(){return Object(a.a)(this,t),Object(u.a)(this,Object(r.a)(t).apply(this,arguments))}return Object(i.a)(t,e),Object(c.a)(t,[{key:"render",value:function(){return o.a.createElement("div",null,o.a.createElement("h1",null,"SyllabusCourseEditor"))}}]),t}(s.Component);t.default=p}}]);
+//# sourceMappingURL=10.c6b312d8.chunk.js.map
