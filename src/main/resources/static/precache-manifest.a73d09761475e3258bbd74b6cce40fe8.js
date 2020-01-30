a28008ab9138e8e9de4c340deccf122be2065e84
kmtusher97 <kmtusher97@gmail.com>
2020-01-30 20:01:14 +0600

    React Front End

---
+++ src/main/resources/static/precache-manifest.a73d09761475e3258bbd74b6cce40fe8.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64a3267040276776fab4abafa97409fe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4423195e4ff07ba7a6fd",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "0805fdb29395c35f8bb6",
+    "url": "/static/css/main.fbf053fa.chunk.css"
+  },
+  {
+    "revision": "691b08b723a843cb19e6",
+    "url": "/static/js/10.c6b312d8.chunk.js"
+  },
+  {
+    "revision": "08b673e4517f6efdc058",
+    "url": "/static/js/11.202f13b1.chunk.js"
+  },
+  {
+    "revision": "c53c7a30745ebc480b27",
+    "url": "/static/js/12.bb0ab6a0.chunk.js"
+  },
+  {
+    "revision": "4423195e4ff07ba7a6fd",
+    "url": "/static/js/2.39040c71.chunk.js"
+  },
+  {
+    "revision": "6951e5822a376f3683b989464a621d5c",
+    "url": "/static/js/2.39040c71.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e14d381712b834e5eebe",
+    "url": "/static/js/3.e5de3876.chunk.js"
+  },
+  {
+    "revision": "3bc0ea97779ae1fd46bc",
+    "url": "/static/js/4.8d21c54f.chunk.js"
+  },
+  {
+    "revision": "842974c15ff66c3cbfa0",
+    "url": "/static/js/5.180cfc5e.chunk.js"
+  },
+  {
+    "revision": "c032178e2b58cd03d402",
+    "url": "/static/js/6.22243a63.chunk.js"
+  },
+  {
+    "revision": "36606b1dc57a9b863824",
+    "url": "/static/js/7.ec9e3b67.chunk.js"
+  },
+  {
+    "revision": "b96395e545c31dfcffbd",
+    "url": "/static/js/8.06583331.chunk.js"
+  },
+  {
+    "revision": "71afdc36fd679f88ae6f",
+    "url": "/static/js/9.e68c013b.chunk.js"
+  },
+  {
+    "revision": "0805fdb29395c35f8bb6",
+    "url": "/static/js/main.d6b9e67f.chunk.js"
+  },
+  {
+    "revision": "0156a2a995a5ebeefd45",
+    "url": "/static/js/runtime-main.f5f76bf4.js"
+  },
+  {
+    "revision": "5e2a2de10de28f92a470b2c95055139e",
+    "url": "/static/media/Jahangirnagar_University_logo.5e2a2de1.png"
+  }
+]);
