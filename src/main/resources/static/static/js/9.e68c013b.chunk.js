077bc1b92344bac7e85a392733f94ef64c1a629e
kmtusher97 <kmtusher97@gmail.com>
2020-01-30 19:52:23 +0600

    mvn clean install

--- src/main/resources/static/static/js/9.e68c013b.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpcseju-office-management-app"]=this["webpackJsonpcseju-office-management-app"]||[]).push([[9],{85:function(e,t,n){"use strict";n.r(t);var a=n(8),c=n(9),u=n(12),i=n(10),r=n(11),s=n(0),p=n.n(s),l=function(e){function t(){return Object(a.a)(this,t),Object(u.a)(this,Object(i.a)(t).apply(this,arguments))}return Object(r.a)(t,e),Object(c.a)(t,[{key:"render",value:function(){return p.a.createElement("div",null,p.a.createElement("h1",null,"Syllabus"))}}]),t}(s.Component);t.default=l}}]);
-//# sourceMappingURL=9.e68c013b.chunk.js.map
