3b582624bc49ca1fa61e86e3c4472413711f5d4a
Kamrul Hasan Tusher <kmtusher97@gmail.com>
2020-03-23 00:49:29 +0600

    Test Deploy v25

---
+++ src/main/resources/static/precache-manifest.2cf0be4503e854f3e30609e9a43b6752.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb3b8d5d6dd0f5337d3d3135bf0f5a8d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c74cbd0d03a342b69a8d",
+    "url": "/static/css/4.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "db9d84e294dbbdea37d7",
+    "url": "/static/css/5.1a755e9d.chunk.css"
+  },
+  {
+    "revision": "d2f7ec9ad848792364d0",
+    "url": "/static/css/main.eab24d3c.chunk.css"
+  },
+  {
+    "revision": "b81867d7f96603de11fa",
+    "url": "/static/js/0.e2d90213.chunk.js"
+  },
+  {
+    "revision": "e9d4405a7035f7379019",
+    "url": "/static/js/1.53658d69.chunk.js"
+  },
+  {
+    "revision": "4ff4d4ecf0b8e2e1c388",
+    "url": "/static/js/10.fdb1cf75.chunk.js"
+  },
+  {
+    "revision": "bb9364c0b6decd17d915",
+    "url": "/static/js/11.8a40cb79.chunk.js"
+  },
+  {
+    "revision": "fa259013a1916b0fba99",
+    "url": "/static/js/12.e9b7814f.chunk.js"
+  },
+  {
+    "revision": "c74cbd0d03a342b69a8d",
+    "url": "/static/js/4.01932d21.chunk.js"
+  },
+  {
+    "revision": "f6cc7459d28ce567f62476c00613afbc",
+    "url": "/static/js/4.01932d21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db9d84e294dbbdea37d7",
+    "url": "/static/js/5.a64bb4c7.chunk.js"
+  },
+  {
+    "revision": "56de1814cff2fa261c36",
+    "url": "/static/js/6.1c84bed3.chunk.js"
+  },
+  {
+    "revision": "79dd5e707a9b46ae0e2a",
+    "url": "/static/js/7.54076dd0.chunk.js"
+  },
+  {
+    "revision": "25bd9a6df2c4ede4e1ca",
+    "url": "/static/js/8.a90d03f7.chunk.js"
+  },
+  {
+    "revision": "c0531367c79031aade84",
+    "url": "/static/js/9.a7123f25.chunk.js"
+  },
+  {
+    "revision": "d2f7ec9ad848792364d0",
+    "url": "/static/js/main.a1256fcc.chunk.js"
+  },
+  {
+    "revision": "9dcaa3279d98c87f2c1f",
+    "url": "/static/js/runtime-main.82e710dd.js"
+  },
+  {
+    "revision": "5e2a2de10de28f92a470b2c95055139e",
+    "url": "/static/media/Jahangirnagar_University_logo.5e2a2de1.png"
+  }
+]);
--- src/main/resources/static/service-worker.js
+++ src/main/resources/static/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ffcda43c6116713665e876af483f706d.js"
+  "/precache-manifest.2cf0be4503e854f3e30609e9a43b6752.js"
 );
 
 self.addEventListener('message', (event) => {
